946259af29b6d429ee701306f331f4238eb69e66
tete-coutard <theocoutard50@gmail.com>
2026-04-08 14:16:02 +0200

    feat: embed public supabase config in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: "https://nqboedyhlmyxyefjkshg.supabase.co",
+    NEXT_PUBLIC_SUPABASE_ANON_KEY:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5xYm9lZHlobG15eHllZmprc2hnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ5MjM0ODEsImV4cCI6MjA5MDQ5OTQ4MX0.qp8IjE9-MRZFKviebneQU6rLP_PRp3ma673HBpTUDf4",
+    NEXT_PUBLIC_APP_URL: "https://iralink-dashboard.vercel.app",
+  },
+};
 
 export default nextConfig;
